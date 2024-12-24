d9f088cc91fd65d2480e1aa8729790a78a496759
Andreas Johansson <glanshammar70@gmail.com>
2024-12-24 12:19:54 +0100

    Added ForumState service

--- ForumBlazor/Program.cs
+++ ForumBlazor/Program.cs
@@ -15,6 +15,7 @@ public class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddSingleton<ForumState>();
         builder.Services.AddBlazoredLocalStorage();
         builder.Services.AddAuthorizationCore();
         builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
