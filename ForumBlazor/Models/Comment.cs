cc089ef093aec81c175db641fc9315efb4fb8c19
Andreas Johansson <glanshammar70@gmail.com>
2024-12-24 12:19:12 +0100

    Added Comment model

--- ForumBlazor/Models/Comment.cs
+++ ForumBlazor/Models/Comment.cs
@@ -1,6 +1,19 @@
 ﻿namespace ForumBlazor.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Comment
 {
-    
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Comment content is required")]
+    [StringLength(5000, ErrorMessage = "Content cannot exceed 5000 characters.")]
+    public string Content { get; set; }
+
+    public string AuthorId { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int PostId { get; set; }
+    public Post Post { get; set; }
 }
